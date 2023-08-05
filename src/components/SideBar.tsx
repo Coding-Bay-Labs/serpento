49a836f0d4367908315e1a2b13c692fe48acc500
nickolasoverlay <nickolaskyryliuk@gmail.com>
2023-08-05 22:03:39 +0300

    Classes - add navbar, use image for a site background

---
+++ src/components/SideBar.tsx
@@ -0,0 +1,5 @@
+const SideBar = () => {
+  return <div className="fixed h-screen w-80 bg-stone-800"></div>;
+};
+
+export default SideBar;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,24 @@
 import { type AppType } from "next/app";
 import { api } from "@/utils/api";
+
+import AppBar from "@/components/AppBar";
+import AppContainer from "@/components/AppContainer";
+import SideBar from "@/components/SideBar";
+
 import "@/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <AppBar />
+      <div className="flex">
+        <SideBar />
+        <AppContainer>
+          <Component {...pageProps} />
+        </AppContainer>
+      </div>
+    </>
+  );
 };
 
 export default api.withTRPC(MyApp);
