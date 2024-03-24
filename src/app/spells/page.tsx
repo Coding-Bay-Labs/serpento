42e3221e3bea43df9517e41fec256848c9161d16
Nickolas Kyryliuk <nickolaskyryliuk@gmail.com>
2024-03-24 15:39:48 +0200

    Fix page type

--- src/app/spells/page.tsx
+++ src/app/spells/page.tsx
@@ -2,8 +2,9 @@ import { getFirstSpellId } from "@/server/api/hygraph/spell";
 import Details from "@/components/spells/Details";
 import SearchPanel from "@/components/spells/SearchPanel";
 
-export const Spells = async () => {
+export default async function  Spells() {
   const spellId = await getFirstSpellId();
+
   return (
     <>
       <h1 className="text-3xl font-semibold text-white">Spells</h1>
